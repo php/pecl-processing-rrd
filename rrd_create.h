038381b746d3a444536c9b6e9b4eb6035d4cec45
Miroslav Kubelik <koubel@php.net>
2011-01-03 12:32:45 +0000

    Tagging the 0.9.0 release
    git-svn-id: http://svn.php.net/repository/pecl/rrd/tags/rrd-0.9.0@307026 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ rrd_create.h
@@ -0,0 +1,30 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2011 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt.                                 |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Miroslav Kubelik <koubel@php.net>                            |
+  +----------------------------------------------------------------------+
+*/
+
+#ifndef RRD_CREATE_H
+#define RRD_CREATE_H
+
+void rrd_create_minit(TSRMLS_DC);
+PHP_FUNCTION(rrd_create);
+
+ZEND_BEGIN_ARG_INFO(arginfo_rrd_create, 0)
+	ZEND_ARG_INFO(0, file)
+	ZEND_ARG_INFO(0, options)
+ZEND_END_ARG_INFO()
+
+#endif  /* RRD_CREATE_H */
