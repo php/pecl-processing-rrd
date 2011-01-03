dd65661540f6d0efb3f926d0f8bff863066c05ca
Miroslav Kubelik <koubel@php.net>
2011-01-03 11:36:31 +0000

    prepare for 0.9.0 release
    git-svn-id: http://svn.php.net/repository/pecl/rrd/trunk@307022 c90b9560-bf6c-de11-be94-00142212c4b1

--- php_rrd.h
+++ php_rrd.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2010 The PHP Group                                |
+  | Copyright (c) 1997-2011 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -22,7 +22,7 @@
 extern zend_module_entry rrd_module_entry;
 #define phpext_rrd_ptr &rrd_module_entry
 
-#define PHP_RRD_VERSION "0.9"
+#define PHP_RRD_VERSION "0.9.0"
 
 #ifdef ZTS
 #include "TSRM.h"
