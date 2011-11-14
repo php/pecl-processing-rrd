5980ffbc8d34c2be3a1a90f578dce4d237a92a2e
Miroslav Kubelik <koubel@php.net>
2011-11-14 17:21:20 +0000

    license changed to the BSD license
    git-svn-id: http://svn.php.net/repository/pecl/rrd/trunk@319181 c90b9560-bf6c-de11-be94-00142212c4b1

--- rrd_info.h
+++ rrd_info.h
@@ -1,20 +1,12 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2011 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt.                                 |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author: Miroslav Kubelik <koubel@php.net>                            |
-  +----------------------------------------------------------------------+
-*/
+/**
+ * PHP bindings to the rrdtool
+ *
+ * This source file is subject to the BSD license that is bundled
+ * with this package in the file LICENSE.
+ * ---------------------------------------------------------------
+ *  Author: Miroslav Kubelik <koubel@php.net>
+ * ---------------------------------------------------------------
+ */
 
 #ifndef RRD_INFO_H
 #define RRD_INFO_H
